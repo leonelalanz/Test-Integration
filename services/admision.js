bbfaef16f59f90e99f01b804e5de5049cac0daa8
nairelyhz <njhs694@gmail.com>
2019-08-10 13:32:24 -0400

    Creada peticion Post admision: recepcion de datos y respuesta del servidor

--- services/admision.js
+++ services/admision.js
@@ -1,7 +1,7 @@
 /**
  * Importacion de paquetes Request, SHA-1
  */
-
+const bodyParser = require('body-parser');
  const request = require("request-promise");
 var sha1 = require('sha1');
 module.exports = function(app){
@@ -17,4 +17,28 @@ module.exports = function(app){
 
         });
       });
+      app.use(bodyParser.urlencoded({ extended: false }));
+      app.use(bodyParser.json());
+      app.post('/admision', function (req, res) {
+        if(!req.body.nombre || !req.body.apellido || !req.body.correo) {
+         respuesta = {
+          error: true,
+          codigo: 502,
+          mensaje: 'El campo nombre, apellido y correo son requeridos'
+         };
+        } else {
+          usuario = {
+           nombre: req.body.nombre,
+           apellido: req.body.apellido,
+           correo : req.body.correo
+          };
+          respuesta = {
+           status: true,
+           codigo: 200,
+           mensaje: 'Usuario creado',
+           data: usuario
+          };
+        };
+        res.send(respuesta);
+       });
 }
