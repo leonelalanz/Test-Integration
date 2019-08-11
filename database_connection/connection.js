4b6818b18b3077e307f29199347f960fe50ae938
nairelyhz <njhs694@gmail.com>
2019-08-10 22:45:56 -0400

    conexion con la base de datos remota y verificacion de funcionalidad de peticion POST

--- database_connection/connection.js
+++ database_connection/connection.js
@@ -1,82 +1,110 @@
+/**
+ * Importacion de módulos
+ *    MySql
+ *    FS
+ */
 var mysql = require('mysql');
-var lectura = require('fs-js');
-
+var fs = require('fs');
 var connection;
+
 module.exports ={
-    init : function(){
-        lectura.open('./arch.txt', 'r', (err, fd) => {
-            if (err) {
-              if (err.code === 'ENOENT') {
-                console.error('myfile does not exist');
-                return;
-              }
-           
-              throw err;
-            }
-           
-            readMyData(fd);
-          });
-        connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'prueba_admision',
-            port: 3306
-         });
-        connection.connect(function(error){
-            if(error){
-               throw error;
-            }else{
-               console.log('Conexion correcta.');
-            }
-         });
-     },
-     close : function(){
-        connection.end();
-     },
-     add : async function(info,oResponse){
-        connection =  mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'prueba_admision',
-            port: 3306
+  /**
+   * Inicializacion de conexión con la DB
+   * @param {*} infoDB 
+   */
+   init : function(infoDB){
+      return new Promise((resolve,reject)=>{
+         connection =  mysql.createConnection({
+            host: infoDB[0],
+            user: infoDB[2],
+            password: infoDB[3],
+            database: infoDB[4],
+            port: infoDB[1]
          });
          connection.connect(function(error){
             if(error){
-               return {
+               reject( {
                   status: false,
                   codigo: 500 ,
                   mensaje: "error",
                   data: error
-               };
+               });
             }else{
-               console.log('Conexion correcta.');
+               resolve('Conexion correcta.');
             }
          });
-        var sSQLCreate =  "INSERT INTO persona (nombre, apellido, correo) VALUES (";
-        sSQLCreate += "'" + info.nombre + "', ";
-        sSQLCreate += "'" + info.apellido + "', ";
-        sSQLCreate += "'" + info.correo + "')";
-         return new Promise( ( resolve, reject ) => {
-            connection.query( sSQLCreate, function ( err, rows ,cols) {
-                if ( err )
-                    return reject( {
-                       status: false,
-                       codigo: 200,
-                        mensaje: 'Usuario no creado',
-                       data: err
-                    });
-                info.id = rows.insertId;
-                resolve( {
-                   status: true,
-                   codigo: 200,
-                   mensaje: 'Usuario creado',
-                   data: info
-                } );
-            } );
-           
-        } );
-     }
+      });
+   },
+
+   /**
+   * Cierre de la conexión con la bd
+   */
+   close : function(){
+      connection.end();
+   },
+
+   /**
+   * Insert en la DB
+   * @param {*} info 
+   * @param {*} oResponse 
+   */
+   add : async function(info,oResponse){
+
+      await this.getDBParameter().then(async (response)=>{
+         infoDB = response;
+         await this.init(response).then((response)=>{
+            console.log(response);
+         })
+      },(err)=>{
+         return{
+            status: false,
+            codigo: 500 ,
+            mensaje: "error",
+            data: error
+         };
+      });
+      var sSQLCreate =  "INSERT INTO persona (nombre, apellido, correo) VALUES (";
+      sSQLCreate += "'" + info.nombre + "', ";
+      sSQLCreate += "'" + info.apellido + "', ";
+      sSQLCreate += "'" + info.correo + "')";
+      return new Promise( ( resolve, reject ) => {
+         connection.query( sSQLCreate, function ( err, rows ,cols) {
+            if ( err )
+               reject( {
+                  status: false,
+                  codigo: 200,
+                  mensaje: 'Usuario no creado',
+                  data: err
+               });
+            info.id = rows.insertId;
+            resolve( {
+               status: true,
+               codigo: 200,
+               mensaje: 'Usuario creado',
+               data: info
+            });
+         });
+      });
+   },
+   /**
+   * Parámetros de conexión con la DB 
+   */
+   getDBParameter: function(){
+      return new Promise ((resolve,reject)=>{
+         var infoDB=[];
+         fs.readFile("./resources/database.properties","utf8",(err,list)=>{
+            if(err){
+               reject(err);
+            }else{
+               const lines = list.split("\n");
+               lines.forEach(element => {
+                  infoLine = element.split(" ");
+                  infoDB.push(infoLine[1]);
+               });
+               resolve(infoDB);
+            }
+         });
+      });
+   }
 
 };
--- services/admision.js
+++ services/admision.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser');
 
 var sha1 = require('sha1');
 module.exports = function(app){
+  /**
+   * Peticion GET
+   */
     app.get('/admision', function (req, res) {
         request({
             uri: "http://jsonplaceholder.typicode.com/albums",
@@ -16,29 +19,31 @@ module.exports = function(app){
                 album.hash = sha1(album.title);
             });
             res.send(albums);
-
         });
       });
-      app.use(bodyParser.urlencoded({ extended: false }));
-      app.use(bodyParser.json());
-      app.post('/admision',async function (req, res) {
-        if(!req.body.nombre || !req.body.apellido || !req.body.correo) {
-         respuesta = {
+
+  /**
+   * Peticion Post
+   */
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.post('/admision',async function (req, res) {
+      if(!req.body.nombre || !req.body.apellido || !req.body.correo) {
+        respuesta = {
           error: true,
           codigo: 502,
           mensaje: 'El campo nombre, apellido y correo son requeridos'
-         };
-        } else {
-          usuario = {
-           nombre: req.body.nombre,
-           apellido: req.body.apellido,
-           correo : req.body.correo
-          };
-          await connection_db.add(usuario,res).then( (respuesta)=>{
-            res.send(respuesta);
-
-          });
- 
         };
-      });
+      } else {
+        usuario = {
+          nombre: req.body.nombre,
+          apellido: req.body.apellido,
+          correo : req.body.correo
+        };
+        await connection_db.add(usuario,res).then( (respuesta)=>{
+          res.send(respuesta);
+          connection_db.close();
+        });
+      };
+    });
 }
